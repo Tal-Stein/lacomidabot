1d14cd72c9947d8286aaad4dd772ade334bbc68b
Tal <tal.stein@gmail.com>
2022-10-14 14:22:45 +0300

    removed img

--- app.py
+++ app.py
@@ -35,13 +35,7 @@ def respond():
 
    else:
        try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+           bot.sendMessage(chat_id=chat_id, text="Unknown command", reply_to_message_id=msg_id)
        except Exception:
            # if things went wrong
            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
